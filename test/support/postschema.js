de3479290b62d560c912eec3a2b872626e4a1fe1
Azmi Makarima <amy.azmim@gmail.com>
2017-12-15 21:36:22 +0700

    v3.0.0

---
+++ test/support/postschema.js
@@ -0,0 +1,92 @@
+const postSchema = {
+    title: 'post schema',
+    type: 'object',
+    required: [
+        'provider',
+        'title',
+        'episode',
+        'url',
+        'thumbnailUrl',
+        'series',
+        'seriesUrl',
+        'releasedAt',
+        'downloads',
+    ],
+    properties: {
+        provider: {
+            type: 'object',
+            required: ['name', 'url'],
+            properties: {
+                name: {
+                    type: 'string',
+                },
+                url: {
+                    type: 'string',
+                    format: 'uri',
+                },
+            },
+        },
+        postId: {
+            type: 'integer',
+        },
+        title: {
+            type: 'string',
+        },
+        episode: {
+            type: 'integer',
+        },
+        url: {
+            type: 'string',
+            format: 'uri',
+        },
+        thumbnailUrl: {
+            type: 'string',
+            format: 'uri',
+        },
+        series: {
+            type: 'string',
+        },
+        seriesUrl: {
+            type: 'string',
+            format: 'uri',
+        },
+        description: {
+            type: 'string',
+        },
+        releasedAt: {
+            type: 'string',
+            format: 'date-time',
+        },
+        downloads: {
+            type: 'array',
+            minItems: 1,
+            items: {
+                type: 'object',
+                required: ['title', 'sources'],
+                properties: {
+                    title: {
+                        type: 'string',
+                    },
+                    sources: {
+                        type: 'array',
+                        items: {
+                            type: 'object',
+                            required: ['name', 'url'],
+                            properties: {
+                                name: {
+                                    type: 'string',
+                                },
+                                url: {
+                                    type: 'string',
+                                    format: 'uri',
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+};
+
+module.exports = postSchema;
